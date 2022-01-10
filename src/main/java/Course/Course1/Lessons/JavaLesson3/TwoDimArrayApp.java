e55956f4c94f5eed50f97ab3025a98ec7c905a54
ZagAlex <zagor85@bk.ru>
2022-01-10 19:09:34 +0300

    NewBranchLesson3

--- src/main/java/Course/Course1/Lessons/JavaLesson3/TwoDimArrayApp.java
+++ src/main/java/Course/Course1/Lessons/JavaLesson3/TwoDimArrayApp.java
@@ -44,5 +44,17 @@ public class TwoDimArrayApp {
         for (int i = 0; i < newArray.length; i++) {
             System.out.println(Arrays.toString(newArray[i]));
         }
+        System.out.println();
+        System.out.println();
+
+        int[][] arr = {{1, 2}, {3, 4}, {5, 6}};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+        char[] chars = "Java".toCharArray();
+        System.out.println(chars[2]);
+
+        System.out.println("Java".charAt(3));
     }
 }
