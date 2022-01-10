1528d991262d6af795855779a6bc1601b9fc296a
ZagAlex <zagor85@bk.ru>
2022-01-10 18:54:44 +0300

    NewBranchLesson3

--- src/main/java/Course/Course1/Lessons/JavaLesson3/TwoDimArrayApp.java
+++ src/main/java/Course/Course1/Lessons/JavaLesson3/TwoDimArrayApp.java
@@ -18,5 +18,31 @@ public class TwoDimArrayApp {
             System.out.println(Arrays.toString(array[i]));
         }
         System.out.println(Arrays.toString(array));
+
+        int[] subArray = array[2];
+        System.out.println(Arrays.toString(subArray));
+        int[] subArray1 = array[1];
+        System.out.println(Arrays.toString(subArray1));
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        int[][] newArray = new int[5][];
+        System.out.println(Arrays.toString(newArray));
+
+        newArray[0] = new int[]{1, 2, 3};
+        newArray[1] = new int[]{1, 2, 3, 4, 5};
+        newArray[2] = new int[]{1};
+        newArray[3] = new int[]{1, 1};
+
+        System.out.println(Arrays.toString(newArray));
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(Arrays.toString(newArray[i]));
+        }
     }
 }
