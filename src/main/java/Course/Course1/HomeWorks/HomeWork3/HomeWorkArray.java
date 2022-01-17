c718be5d0e52067c19523f570eb769641dd63612
ZagAlex <zagor85@bk.ru>
2022-01-17 21:09:32 +0300

    Parameters

--- src/main/java/Course/Course1/HomeWorks/HomeWork3/HomeWorkArray.java
+++ src/main/java/Course/Course1/HomeWorks/HomeWork3/HomeWorkArray.java
@@ -4,23 +4,27 @@ import java.util.Arrays;
 
 public class HomeWorkArray {
     public static void main(String[] args) {
+        int[] array = new int[]{1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0};
+        arrayElementsReplace(array);
+        System.out.println();
 
-        arrayElementsReplace();
-        System.out.println();
-        arrayFrom1To100();
+        arrayFrom1To100(100);
         System.out.println();
         System.out.println();
-        arrayElementsLessThenSix();
+
+        int[] array2 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        arrayElementsLessThenSix(array2);
         System.out.println();
-        squareArray();
+
+        squareArray(5,5);
         System.out.println();
         System.out.println();
-        int[] array = fillArray(4, 5);
+
+        int[] arr = fillArray(4, 5);
 
     }
 
-    public static void arrayElementsReplace() {
-        int[] array = {1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0};
+    public static void arrayElementsReplace(int[] array) {
         System.out.println(Arrays.toString(array));
         for (int i = 0; i < array.length; i++) {
             if (array[i] == 0) {
@@ -32,8 +36,8 @@ public class HomeWorkArray {
         System.out.println(Arrays.toString(array));
     }
 
-    public static void arrayFrom1To100() {
-        int[] array1 = new int[100];
+    public static void arrayFrom1To100(int elements) {
+        int[] array1 = new int[elements];
         int value = 1;
         for (int i = 0; i < array1.length; i++) {
             array1[i] = value;
@@ -43,8 +47,8 @@ public class HomeWorkArray {
 
     }
 
-    public static void arrayElementsLessThenSix() {
-        int[] array2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    public static void arrayElementsLessThenSix(int[] array2) {
+
         System.out.println(Arrays.toString(array2));
         for (int i = 0; i < array2.length; i++) {
             if (array2[i] < 6) {
@@ -53,9 +57,10 @@ public class HomeWorkArray {
         }
         System.out.println(Arrays.toString(array2));
     }
-//Расскажешь на уроке, как заполнять вторую диагональ поподробнее?
-    public static void squareArray() {
-        int[][] array3 = new int[5][5];
+
+
+    public static void squareArray(int row, int column) {
+        int[][] array3 = new int[row][column];
         for (int i = 0; i < array3.length; i++) {
             for (int j = 0; j < array3[i].length; j++) {
                 if (array3[i] == array3[j]) {
