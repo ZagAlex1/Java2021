7ecbf81ec4897a02d1a92988126b91aff6d13907
ZagAlex <zagor85@bk.ru>
2022-02-01 20:12:25 +0300

    HW6

--- src/main/java/Course/Course1/Lessons/JavaLesson6/Bike.java
+++
@@ -1,9 +0,0 @@
-package Course.Course1.Lessons.JavaLesson6;
-
-public class Bike extends Transport {
-    int wheelsNumber;
-
-    public Bike(int wheelsNumber) {
-        this.wheelsNumber = wheelsNumber;
-    }
-}
--- src/main/java/Course/Course1/Lessons/JavaLesson6/Transport.java
+++
@@ -1,8 +0,0 @@
-package Course.Course1.Lessons.JavaLesson6;
-
-public class Transport {
-
-    void printName() {
-        System.out.println("Transport");
-    }
-}
