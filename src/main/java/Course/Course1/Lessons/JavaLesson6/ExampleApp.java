6b96932598b8c933cf5a1eb9659d101331be37d8
ZagAlex <zagor85@bk.ru>
2022-02-15 19:44:25 +0300

    HW7

--- src/main/java/Course/Course1/Lessons/JavaLesson6/ExampleApp.java
+++
@@ -1,9 +0,0 @@
-package Course.Course1.Lessons.JavaLesson6;
-
-public class ExampleApp {
-    public static void main(String[] args) {
-        Example example = new Example(10, 20);
-        example.setA(100);
-        example.info();
-    }
-}
--- src/main/java/Course/Course1/Lessons/JavaLesson6/Invention.java
+++
@@ -1,6 +0,0 @@
-package Course.Course1.Lessons.JavaLesson6;
-
-public abstract class Invention {
-
-    public abstract void printInventionDate();
-}
